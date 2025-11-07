a2e82efad8f38ef55e7603450987ab1836c24d2f
asli-nobita <hamzanajfi2765@gmail.com>
2025-11-07 18:27:49 +0530

    echo

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    std::string exit_msg = "exit 0"; 
+    std::string exit_msg = "exit 0";
 
     while (1) {
         std::cout << "$ ";
         std::string input;
-        std::getline(std::cin, input); 
-        if(input.compare(exit_msg) == 0) { 
+        std::getline(std::cin, input);
+        if (input == exit_msg) {
             // std::cout << "Exiting...\n";  
-            std::exit(0); 
+            std::exit(0);
         }
-        std::cout << input << ": command not found\n"; 
+        // check if the first word is "echo" 
+        std::istringstream iss(input);
+        std::string first;
+        iss >> first;
+        if (first == "echo") {
+            std::string rest;
+            std::getline(iss, rest);
+            auto pos = rest.find_first_not_of(' ');
+            if (pos != std::string::npos) {
+                rest.erase(0, pos);
+            }
+            else {
+                rest.clear();
+            }
+            std::cout << rest << "\n";
+            continue;
+        }
+        else std::cout << input << ": command not found\n";
     }
 }
