04565fd169d56caa085bcc382548133027c6c182
asli-nobita <hamzanajfi2765@gmail.com>
2025-11-07 14:58:50 +0530

    stage2

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,13 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+    // TODO: Uncomment the code below to pass the first stage
+    std::cout << "$ ";
+    std::string input;
+    std::cin >> input;
+    std::cout << input << ": command not found";
 }
